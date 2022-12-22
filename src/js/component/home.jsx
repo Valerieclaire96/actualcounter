26227fb1893ba0da60a9156be0ad98f9fa577722
Valerieclaire96 <Valerieclaire96@gmail.com>
2022-12-22 18:42:18 +0000

    finished simple counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,25 +9,25 @@ import { AiOutlineClockCircle } from "react-icons/ai";
 function Home(props) {
   return (
     <div className="container">
-      <h1 className="counter text-white border rounded border-white">
+      <h1 className="icon num">
         <AiOutlineClockCircle />
       </h1>
-      <h1 className="counter text-white border rounded border-white">
+      <h1 className="num">
         {Math.floor(props.sec / 100000) % 10}
       </h1>
-      <h1 className="counter text-white border rounded border-white">
+      <h1 className="num">
         {Math.floor(props.sec / 10000) % 10}
       </h1>
-      <h1 className="counter text-white border rounded border-white">
+      <h1 className="num">
         {Math.floor(props.sec / 1000) % 10}
       </h1>
-      <h1 className="counter text-white border rounded border-white">
+      <h1 className="num">
         {Math.floor(props.sec / 100) % 10}
       </h1>
-      <h1 className="counter text-white border rounded border-white">
+      <h1 className="num">
         {Math.floor(props.sec / 10) % 10}
       </h1>
-      <h1 className="counter text-white border rounded border-white">
+      <h1 className="num">
         {Math.floor(props.sec / 1) % 10}
       </h1>
     </div>
